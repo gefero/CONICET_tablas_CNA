01a5b602d3e33925afeb7d5cd425961d24c8cb7f
German Rosati <german.rosati@gmail.com>
2022-06-10 12:27:33 -0300

    Modifico nombres de archivo y agrego diccionario de la tabla

--- src/2_uso_suelo_agregado_coeficientes.R
+++ src/2_uso_suelo_agregado_coeficientes.R
@@ -48,49 +48,6 @@ uso2018 <- uso2018 %>%
         mutate(demanda_agg = case_when(
                 is.na(demanda_agg) ~ 'S/D',
                 TRUE  ~ demanda_agg))
-uso2018 %>%
-        group_by(grupo_cultivo, cultivo, demanda_agg) %>%
-        summarise(has = sum(valor)) %>%
-        mutate(prop_sobre_grupo = has/sum(has)*100) %>%
-        ungroup() %>%
-        mutate(prop_sobre_total = has/sum(has)*100) %>%
-        filter(demanda_agg=='S/D')
-
-sin_datos2018 <- uso2018 %>%
-        #filter(grupo_cultivo != 'Bosques y montes implantados') %>%
-        group_by(provincia, grupo_cultivo, cultivo, demanda_agg) %>%
-        summarise(has = sum(valor)) %>%
-        #mutate(prop_sobre_grupo = has/sum(has)) %>%
-        ungroup() %>%
-        group_by(provincia) %>%
-        mutate(prop_sobre_provincia = has/sum(has)) %>%
-        ungroup() %>%
-        mutate(prop_sobre_total = has/sum(has)) %>%
-        filter(demanda_agg == 'S/D') %>%
-        arrange(provincia, desc(prop_sobre_provincia)) %>%
-        group_by(provincia) %>%
-        top_n(wt=prop_sobre_provincia, 30)
-
-openxlsx::write.xlsx(sin_datos2018, './data/outputs/uso_sin_datos_coeficientes_2018.xlsx')
-
-sin_datos2018 %>%
-        gt(groupname_col = c("provincia")) %>%
-        fmt_number(has, decimals=1) %>%
-        fmt_percent(columns=c("prop_sobre_provincia", "prop_sobre_total"),
-                    decimals = 3) %>%
-        summary_rows(
-                groups = TRUE,
-                columns = c(has),
-                fns = list(
-                        total = "sum"),
-        ) %>%
-        summary_rows(
-                groups = TRUE,
-                columns = c(prop_sobre_provincia, prop_sobre_total),
-                fns = list(
-                        total = "sum"),
-                formatter = fmt_percent,
-        )
 
 write_csv(uso2018, './data/proc/uso_suelo_2018_con_coeficientes.csv')
 
@@ -106,7 +63,6 @@ uso2002 <- read_csv('./data/proc/uso_suelo_2002.csv')
 uso2002 <- uso2002 %>%
         mutate(id_fila = row_number()) %>%
         select(id_fila, everything())
-
 
 uso2002 <- uso2002 %>%
         mutate(grupo_cultivo = case_when(
@@ -169,48 +125,13 @@ has2002b<-uso2002 %>%
         group_by(grupo_cultivo) %>%
         summarise(n=sum(valor))
 
+## Cheque consistencias agregadas
 has2002a == has2002b
 
 uso2002 <- uso2002 %>%
         mutate(demanda_agg = case_when(
                 is.na(demanda_agg) ~ 'S/D',
                 TRUE  ~ demanda_agg))
-
-sin_datos2002 <- uso2002 %>%
-        #filter(grupo_cultivo != 'Bosques y montes implantados') %>%
-        group_by(provincia, grupo_cultivo, cultivo, demanda_agg) %>%
-        summarise(has = sum(valor)) %>%
-        #mutate(prop_sobre_grupo = has/sum(has)) %>%
-        ungroup() %>%
-        group_by(provincia) %>%
-        mutate(prop_sobre_provincia = has/sum(has)) %>%
-        ungroup() %>%
-        mutate(prop_sobre_total = has/sum(has)) %>%
-        filter(demanda_agg == 'S/D') %>%
-        arrange(provincia, desc(prop_sobre_provincia)) %>%
-        group_by(provincia) %>%
-        top_n(wt=prop_sobre_provincia, 30)
-
-
-sin_datos2002 %>%
-        gt(groupname_col = c("provincia")) %>%
-        fmt_number(has, decimals=1) %>%
-        fmt_percent(columns=c("prop_sobre_provincia", "prop_sobre_total"),
-                    decimals = 3) %>%
-        summary_rows(
-                groups = TRUE,
-                columns = c(has),
-                fns = list(
-                        total = "sum"),
-        ) %>%
-        summary_rows(
-                groups = TRUE,
-                columns = c(prop_sobre_provincia, prop_sobre_total),
-                fns = list(
-                        total = "sum"),
-                formatter = fmt_percent,
-        )
-openxlsx::write.xlsx(sin_datos2002, './data/outputs/uso_sin_datos_coeficientes_2002.xlsx')
 
 write_csv(uso2002, './data/proc/uso_suelo_2002_con_coeficientes.csv')
 
---
+++ src/3_uso_suelo_checks_cobertura.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+uso2002 <- read_csv('./data/proc/uso_suelo_2002_con_coeficientes.csv')
+uso2018 <- read_csv('./data/proc/uso_suelo_2018_con_coeficientes.csv')
+
+
+
+sin_datos2002 <- uso2002 %>%
+        #filter(grupo_cultivo != 'Bosques y montes implantados') %>%
+        group_by(provincia, grupo_cultivo, cultivo, demanda_agg) %>%
+        summarise(has = sum(valor)) %>%
+        #mutate(prop_sobre_grupo = has/sum(has)) %>%
+        ungroup() %>%
+        group_by(provincia) %>%
+        mutate(prop_sobre_provincia = has/sum(has)) %>%
+        ungroup() %>%
+        mutate(prop_sobre_total = has/sum(has)) %>%
+        filter(demanda_agg == 'S/D') %>%
+        arrange(provincia, desc(prop_sobre_provincia)) %>%
+        group_by(provincia) %>%
+        top_n(wt=prop_sobre_provincia, 30)
+
+
+sin_datos2002 %>%
+        gt(groupname_col = c("provincia")) %>%
+        fmt_number(has, decimals=1) %>%
+        fmt_percent(columns=c("prop_sobre_provincia", "prop_sobre_total"),
+                    decimals = 3) %>%
+        summary_rows(
+                groups = TRUE,
+                columns = c(has),
+                fns = list(
+                        total = "sum"),
+        ) %>%
+        summary_rows(
+                groups = TRUE,
+                columns = c(prop_sobre_provincia, prop_sobre_total),
+                fns = list(
+                        total = "sum"),
+                formatter = fmt_percent,
+        )
+
+openxlsx::write.xlsx(sin_datos2002, './data/outputs/uso_sin_datos_coeficientes_2002.xlsx')
+
+uso2018 %>%
+        group_by(grupo_cultivo, cultivo, demanda_agg) %>%
+        summarise(has = sum(valor)) %>%
+        mutate(prop_sobre_grupo = has/sum(has)*100) %>%
+        ungroup() %>%
+        mutate(prop_sobre_total = has/sum(has)*100) %>%
+        filter(demanda_agg=='S/D')
+
+sin_datos2018 <- uso2018 %>%
+        #filter(grupo_cultivo != 'Bosques y montes implantados') %>%
+        group_by(provincia, grupo_cultivo, cultivo, demanda_agg) %>%
+        summarise(has = sum(valor)) %>%
+        #mutate(prop_sobre_grupo = has/sum(has)) %>%
+        ungroup() %>%
+        group_by(provincia) %>%
+        mutate(prop_sobre_provincia = has/sum(has)) %>%
+        ungroup() %>%
+        mutate(prop_sobre_total = has/sum(has)) %>%
+        filter(demanda_agg == 'S/D') %>%
+        arrange(provincia, desc(prop_sobre_provincia)) %>%
+        group_by(provincia) %>%
+        top_n(wt=prop_sobre_provincia, 30)
+
+openxlsx::write.xlsx(sin_datos2018, './data/outputs/uso_sin_datos_coeficientes_2018.xlsx')
+
+sin_datos2018 %>%
+        gt(groupname_col = c("provincia")) %>%
+        fmt_number(has, decimals=1) %>%
+        fmt_percent(columns=c("prop_sobre_provincia", "prop_sobre_total"),
+                    decimals = 3) %>%
+        summary_rows(
+                groups = TRUE,
+                columns = c(has),
+                fns = list(
+                        total = "sum"),
+        ) %>%
+        summary_rows(
+                groups = TRUE,
+                columns = c(prop_sobre_provincia, prop_sobre_total),
+                fns = list(
+                        total = "sum"),
+                formatter = fmt_percent,
+        )
+
